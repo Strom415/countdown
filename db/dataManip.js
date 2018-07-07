96a5b26b2c6cd156f3bf59f9d19b3963e17377fe
Matthew Strom <strom@Matthews-MacBook-Air.local>
2018-07-07 13:42:44 -0700

    setup webpack config.  defined anagram algos

--- db/dataManip.js
+++ db/dataManip.js
@@ -1,13 +1,80 @@
-var fs = require("fs");
+const fs = require("fs");
 
-var text = fs.readFileSync("./enable1.txt");
-var textArray = (text + '').split("\n");
-textArray = textArray.map(word => word.trim());
-textArray = textArray.filter(word => word.length <= 9);
+const makeDictionary = (file) => {
+  const dictionary = {};
+  let words = fs.readFileSync(file);
+  words = (words + '').split('\n');
+  words = words.map(word => word.trim());
 
-for (let i = 0; i < textArray.length; i++) {
-  console.log(`${i} of ${textArray.length}`);
-  fs.appendFileSync("words.txt", textArray[i] + '\n');
-}
+  for (let i = 0; i < words.length; i++) {
+    dictionary[words[i]] = true;
+  }
+  return dictionary;
+};
 
-console.log(textArray.length);
+const allAnagrams = (string) => {
+  const uniqueOutput = {};
+
+  const anagram = (ana, str) => {
+    if (str === '') { uniqueOutput[ana] = 1; }
+
+    for (let i = 0; i < str.length; i++) {
+      anagram(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+    }
+  };
+
+  anagram('', string);
+
+  return Object.keys(uniqueOutput);
+};
+
+const powerSet = (str) => {
+  const set = [];
+  const hash = {};
+  if (!str) { str = ''; }
+  str = str.split('').sort();
+
+  // recursive through the sub sets
+  const recurse = (strSet) => {
+    const joined = strSet.join('');
+
+    // check if we have visited this combo
+    if (hash[joined]) { return; }
+    hash[joined] = true;
+    set.push(joined);
+
+    // don't recurse to empty set - add it once at the end
+    if (strSet.length === 1) { return; }
+
+    // recurse all subsets
+    for (let i = 0; i < strSet.length; i++) {
+      const subSet = strSet.slice(0, i).concat(strSet.slice(i + 1));
+      recurse(subSet);
+    }
+  };
+  recurse(str);
+
+  return set;
+};
+
+const dictionary = makeDictionary('./dictionary-max-9.txt');
+
+const realAnagrams = (str) => {
+  const matches = [];
+  const pSet = powerSet(str);
+
+  for (let i = 0; i < pSet.length; i++) {
+    const current = allAnagrams(pSet[i]);
+
+    for (let j = 0; j < current.length; j++) {
+      if (dictionary[current[j]]) {
+        matches.push(current[j]);
+      }
+    }
+  }
+
+  return matches.sort((a, b) => b.length - a.length);
+};
+
+const anas = realAnagrams('eepluochc');
+console.log('matches', anas);
--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3087;
 
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, '../public/dist')));
 
 app.listen(port, console.log(`listening on port ${port}`));
