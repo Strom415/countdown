6a9adf8bb31333d2a3eaa0fc0bb24f92925282b3
Matthew Strom <strom@Matthews-MacBook-Air.local>
2019-01-30 15:34:07 -0800

    added postinstall script to package.json which builds bundle

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const db = require('../db/index.js');
 const app = express();
 const port = process.env.PORT || 3087;
 
-app.use(express.static(path.resolve(__dirname, './public/dist')));
+app.use(express.static(path.resolve(__dirname, '../public/dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
